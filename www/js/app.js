5032d91cefe313d6cec250481c2dd00a98251355
djkiourtsis <djkiourtsis@wpi.edu>
2016-09-15 11:34:44 -0400

    Introduce multi-page functionality
    This commit enables the ionic app to use multiple pages.
    Separate pages should be placed in www/templates/ and added
    to www/js/apps.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,25 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller("MainCtrl",function($scope){
+  console.log("Main Controller says: Hello World");
+});
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  
+  .state('main', {
+    url: "/main",
+    templateUrl: "templates/main.html",
+    controller: 'MainCtrl'
+  })
+  
+  .state('page2', {
+    url: "/page2",
+    templateUrl: "templates/page2.html",
+  });
+  
+  $urlRouterProvider.otherwise('/main');
+});
