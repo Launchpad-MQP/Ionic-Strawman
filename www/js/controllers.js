8a052b7370a7a4edd0a1674edbcb6de3296f9e9f
djkiourtsis <djkiourtsis@wpi.edu>
2016-09-22 17:39:50 -0400

    Auto generate level select grid
    This commit modifies the LevelSelectCtrl to use a list of level
    numbers.  Instead of generating a grid for the levels manually,
    ng-repeat will generate the grid using the level list.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,21 +46,7 @@ app.controller("MainCtrl", function($scope) {
 		console.log("Entering level " + levelNum);
 		$state.go("level", {'levelNum': levelNum});
 	}
-	
-  var table = document.getElementsByClassName("levelTable")[0];
-  var height = 5;
-  var width = 4;
-  for (var i=0; i<height; i+=1) {
-    var row = table.insertRow(i);
-    for (var j=0; j<width; j+=1) {
-      var levelNo = i*width+j;
-      var cell = row.insertCell(j);
-      cell.setAttribute("name", j+"_"+i);
-      var btn = document.createElement("button");
-      btn.setAttribute("class", "button button-outline button-calm");
-      btn.innerHTML = levelNo;
-      // btn.setAttribute("onmousedown", "loadLevel("+levelNo+")");
-      cell.appendChild(btn);
-    }
-  }
+    	
+    $scope.myTitle = 'Template';
+    $scope.levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 });
