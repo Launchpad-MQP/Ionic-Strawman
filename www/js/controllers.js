c209caac9e5644399a5839b70cbf89d0f197b188
Bailey <bsheridan@wpi.edu>
2016-09-22 16:18:28 -0400

    Adds basic level functionality
    Adds level screen and a controller. Level select doesn't properly move
    on to levels yet.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,49 @@ app.controller("MainCtrl", function($scope) {
   console.log("Main Controller says: Hello World");
 })
 
-.controller("LevelCtrl", function($scope) {
+.controller("LevelCtrl", function($scope, $state, $stateParams, $ionicPopup) {
+	console.log($stateParams.levelNum);
+	
+	//Creates a popup when the level is complete.
+	$scope.completeLevel = function() {
+		$scope.data = {};
+		
+		var levelOverPopUp = $ionicPopup.show({
+			title: "Level Complete!",
+			scope: $scope,
+			buttons: [
+			{
+				text: 'Level Select',
+				onTap: function(e) {
+					console.log("Back to level select.");
+					$state.go('level_select');
+				}
+				},
+			{
+				text: 'Next',
+				type: 'button-positive',
+				onTap: function(e) {
+					console.log("On to the next level.");
+					$state.go('level', {'levelNum': $stateParams.levelNum+1});
+				}
+				}]
+		});
+	}
+	
+	//Restarts the level.
+	$scope.restart = function() {
+		console.log("Restarts level.");
+		$state.reload();
+	}
+})
+
+.controller("LevelSelectCtrl", function($scope, $state) {
+	
+	$scope.loadLevel = function(levelNum) {
+		console.log("Entering level " + levelNum);
+		$state.go("level", {'levelNum': levelNum});
+	}
+	
   var table = document.getElementsByClassName("levelTable")[0];
   var height = 5;
   var width = 4;
@@ -17,7 +59,7 @@ app.controller("MainCtrl", function($scope) {
       var btn = document.createElement("button");
       btn.setAttribute("class", "button button-outline button-calm");
       btn.innerHTML = levelNo;
-      btn.setAttribute("onmousedown", "loadLevel("+levelNo+")");
+      // btn.setAttribute("onmousedown", "loadLevel("+levelNo+")");
       cell.appendChild(btn);
     }
   }
