934106c6b241e1e42030bc65cff292f70a473e3c
jbzdarkid <jbzdarkid@gmail.com>
2016-09-23 06:46:22 -0400

    Fixes whitespace

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,50 +5,50 @@ app.controller("MainCtrl", function($scope) {
 })
 
 .controller("LevelCtrl", function($scope, $state, $stateParams, $ionicPopup) {
-	console.log($stateParams.levelNum);
+  console.log($stateParams.levelNum);
 
-	//Creates a popup when the level is complete.
-	$scope.completeLevel = function() {
-		$scope.data = {};
+  //Creates a popup when the level is complete.
+  $scope.completeLevel = function() {
+    $scope.data = {};
 
-		var levelOverPopUp = $ionicPopup.show({
-			title: "Level Complete!",
-			scope: $scope,
-			buttons: [
-			{
-				text: 'Level Select',
-				onTap: function(e) {
+    var levelOverPopUp = $ionicPopup.show({
+      title: "Level Complete!",
+      scope: $scope,
+      buttons: [
+      {
+        text: 'Level Select',
+        onTap: function(e) {
           completeLevel($stateParams.levelNum);
-					console.log("Back to level select.");
-					$state.go('level_select');
-				}
-				},
-			{
-				text: 'Next',
-				type: 'button-positive',
-				onTap: function(e) {
+          console.log("Back to level select.");
+          $state.go('level_select');
+        }
+        },
+      {
+        text: 'Next',
+        type: 'button-positive',
+        onTap: function(e) {
           completeLevel($stateParams.levelNum);
-					console.log("On to the next level.");
-					$state.go('level', {'levelNum': $stateParams.levelNum+1});
-				}
-				}]
-		});
-	}
+          console.log("On to the next level.");
+          $state.go('level', {'levelNum': $stateParams.levelNum+1});
+        }
+        }]
+    });
+  }
 
-	//Restarts the level.
-	$scope.restart = function() {
-		console.log("Restarts level.");
-		$state.reload();
-	}
+  //Restarts the level.
+  $scope.restart = function() {
+    console.log("Restarts level.");
+    $state.reload();
+  }
 })
 
 .controller("LevelSelectCtrl", function($scope, $state) {
   console.log("Now in the Level Select");
 
-	$scope.loadLevel = function(levelNum) {
-		console.log("Entering level " + levelNum);
-		$state.go("level", {'levelNum': levelNum});
-	}
+  $scope.loadLevel = function(levelNum) {
+    console.log("Entering level " + levelNum);
+    $state.go("level", {'levelNum': levelNum});
+  }
 
   $scope.myTitle = 'Template';
   $scope.levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
