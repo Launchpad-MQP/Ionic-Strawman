76ff0eb94c53eae9b6d8c03a17ea45058b99cbce
jbzdarkid <jbzdarkid@gmail.com>
2016-10-01 12:02:31 -0400

    Adds a simple code example

---
+++ ionicMQP2016/src/main/java/ionicmqp2016/PrintFragments.java
@@ -0,0 +1,11 @@
+package ionicmqp2016;
+
+import java.util.Iterator;
+public class PrintFragments {
+	public static void processResults (Iterator<String> it) {
+		for (int i=0; it.hasNext(); i++) {
+			String fragment = it.next();
+			System.out.println("Fragment ("+i+"): \n"+fragment);
+		}
+	}
+}
---
+++ ionicMQP2016/src/main/scala/Simple.scala
@@ -0,0 +1,51 @@
+import de.tu_dortmund.cs.ls14.cls.interpreter.ReflectedRepository
+import de.tu_dortmund.cs.ls14.cls.types.syntax._
+import de.tu_dortmund.cs.ls14.cls.types._
+import de.tu_dortmund.cs.ls14.cls.interpreter.combinator
+
+import scala.collection.JavaConverters._
+import ionicmqp2016._;
+import _root_.java.nio.file._
+
+object SimpleNameWhichDoesntMatchFileName extends App {
+  trait SimpleTrait {
+
+    // Defines a list of types. Basically the function prototypes. Includes parameters.
+    // trait Expr
+    // case class BaseFramework ()
+    // case class MainPage ()
+
+    // Very basic returns a string.
+    @combinator object EmptyApp {
+      // def apply() : String = { <java code> }
+      def apply() : String = { return "angular.module('starter', ['ionic'])"; }
+      val semanticType:Type = 'emptyApp;
+    }
+
+    @combinator object MainPage {
+      // def apply() : String = { <java code> }
+      def apply() : String = { return ".run(function ($ionicPlatform){})"; }
+      val semanticType:Type = 'mainPage;
+    }
+
+    @combinator object SimpleApp extends BasicCombinator()
+
+    // Combines the basic EmptyApp with a basic MainPage and returns it. No parameters needed.
+    class BasicCombinator() {
+      // takes a (String) and b (String) returns (String) according to <java code>
+      def apply(a:String, b:String) : String = { return a + '\n' + b; }
+      val semanticType:Type = 'emptyApp =>: 'mainPage =>: 'appWithMainPage;
+    }
+  }
+
+  // Initializes the CLS system
+  val reflectedRepository = ReflectedRepository (new SimpleTrait {})
+
+  // Get the interpreted response from CLS
+  val reply = reflectedRepository.inhabit[String] ('appWithMainPage)
+
+  // Pass the response into our defined output, currently just a printer
+  val it = reply.interpretedTerms.values.flatMap(_._2).iterator
+  PrintFragments.processResults(it.asJava)
+
+}
