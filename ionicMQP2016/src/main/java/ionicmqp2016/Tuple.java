56dbe8e28b06e65973b1248c43141490246cff82
jbzdarkid <jbzdarkid@gmail.com>
2016-11-08 13:30:56 -0500

    Adds working code from george

---
+++ ionicMQP2016/src/main/java/ionicmqp2016/Tuple.java
@@ -0,0 +1,14 @@
+package ionicmqp2016;
+
+public class Tuple {
+	final String a;
+	final String b;
+
+	public Tuple (String a, String b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public String _1() { return a; }
+	public String _2() { return b; }
+}
