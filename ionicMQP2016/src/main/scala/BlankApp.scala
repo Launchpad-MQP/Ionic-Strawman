1bad6bfbef96f62ca9d1736377f21868f46cd7fe
jbzdarkid <jbzdarkid@gmail.com>
2016-11-12 15:19:21 -0500

    Moved & cleaned up

--- ionicMQP2016/src/main/scala/BlankApp.scala
+++ ionicMQP2016/src/main/scala/BlankApp.scala
@@ -59,6 +59,78 @@ object BlankApp extends App {
       val semanticType:Type = 'hangman :&: 'html
     }
 
+    @combinator object HangmanJs {
+      def apply(): String = {
+        """
+  $scope.myLetters = [
+    'hello', 'wolf', 'rat', 'xylophone', 'attention',
+    'school', 'ruling', 'poison', 'tree', 'prison',
+    'abacus', 'toothache', 'short', 'bacon', 'crossroads',
+    'darkness', 'candle', 'quadruple', 'extraordinary', 'declaration'
+  ][$scope.levelNum-1].split("");
+  $scope.guessesLeft = 7;
+  $scope.miss = true;
+
+  $scope.makeGuess = function () {
+    var guess = document.getElementById("letterguess_" + $scope.levelNum).value;
+    var card = document.getElementById("guessed_" + $scope.levelNum);
+
+    console.log("Guess", guess);
+    // Fields are the elements which match the letter guessed.
+    var fields = document.getElementsByClassName(guess);
+    console.log("Fields", fields);
+    for(var i = 0; i<fields.length; i++) {
+        if(!fields[i].className.includes("discovered")) {
+          fields[i].className = "discovered " + $scope.levelNum + " " + guess;
+        }
+        $scope.miss = false;
+    }
+    if($scope.miss) {
+      if(!card.innerHTML.includes(guess))
+        $scope.guessesLeft--;
+      if($scope.guessesLeft===0)
+        $scope.loseLevel();
+    }
+
+    $scope.miss = true;
+    if(!card.innerHTML.includes(guess))
+      card.append(guess + " ");
+    $scope.checkComplete();
+  }
+
+  $scope.loseLevel = function() {
+    $ionicPopup.show({
+      title: "You lose!",
+      buttons: [
+      {
+        text: "Level Select",
+        onTap: function () {
+          console.log("Back to level select.");
+          $state.go("level_select");
+        }
+      },
+      {
+        text: "Retry",
+        type: "button-positive",
+        onTap: function () {
+          console.log("Reloading...");
+          $scope.restart();
+        }
+      }]
+    });
+  }
+
+  $scope.checkComplete = function () {
+    var correct = document.getElementsByClassName("discovered " + $scope.levelNum);
+    console.log(correct.length);
+    if(correct.length === $scope.myLetters.length)
+      $scope.completeLevel();
+  }
+"""
+      }
+      val semanticType:Type = 'hangman :&: 'js
+    }
+
     @combinator object DummyHTML {
       def apply(): String = {
         return """
@@ -94,75 +166,6 @@ object BlankApp extends App {
 """
       }
       val semanticType:Type = gameVar :&: 'html =>: gameVar :&: 'gameHtml
-    }
-
-    @combinator object HangmanJs {
-      def apply(): String = {
-        """	$scope.myLetters = ['hello', 'wolf', 'rat', 'xylophone', 'attention',
-																'school', 'ruling', 'poison', 'tree', 'prison',
-																'abacus', 'toothache', 'short', 'bacon', 'crossroads',
-																'darkness', 'candle', 'quadruple', 'extraordinary', 'declaration'][$scope.levelNum-1].split("");
-	$scope.guessesLeft = 7;
-	$scope.miss = true;
-
-	$scope.makeGuess = function () {
-		var guess = document.getElementById("letterguess_" + $scope.levelNum).value;
-		var card = document.getElementById("guessed_" + $scope.levelNum);
-
-		console.log("Guess", guess);
-		// Fields are the elements which match the letter guessed.
-		var fields = document.getElementsByClassName(guess);
-		console.log("Fields", fields);
-		for(var i = 0; i<fields.length; i++) {
-				if(!fields[i].className.includes("discovered")) {
-					fields[i].className = "discovered " + $scope.levelNum + " " + guess;
-				}
-				$scope.miss = false;
-		}
-		if($scope.miss) {
-			if(!card.innerHTML.includes(guess))
-				$scope.guessesLeft--;
-			if($scope.guessesLeft===0)
-				$scope.loseLevel();
-		}
-
-		$scope.miss = true;
-		if(!card.innerHTML.includes(guess))
-			card.append(guess + " ");
-		$scope.checkComplete();
-	}
-
-	$scope.loseLevel = function() {
-		$ionicPopup.show({
-      title: "You lose!",
-      buttons: [
-      {
-        text: "Level Select",
-        onTap: function () {
-          console.log("Back to level select.");
-          $state.go("level_select");
-        }
-      },
-      {
-        text: "Retry",
-        type: "button-positive",
-        onTap: function () {
-          console.log("Reloading...");
-          $scope.restart();
-        }
-      }]
-    });
-	}
-
-	$scope.checkComplete = function () {
-		var correct = document.getElementsByClassName("discovered " + $scope.levelNum);
-		console.log(correct.length);
-		if(correct.length === $scope.myLetters.length)
-			$scope.completeLevel();
-	}
-"""
-      }
-      val semanticType:Type = 'hangman :&: 'js
     }
 
     @combinator object GameJs {
@@ -660,5 +663,4 @@ angular.module("controllers", ["ionic", "sql"])
   // Pass the response into our defined output, currently just a printer
   val iter = reply.interpretedTerms.values.flatMap(_._2).iterator.asJava
   DirectoryMaker.parseResults(iter)
-
 }
