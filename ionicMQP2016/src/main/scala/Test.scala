8088541517a81fa602014afad6a3579c873d8827
jbzdarkid <jbzdarkid@gmail.com>
2016-11-12 13:55:14 -0500

    A bit of cleanup & comments

--- ionicMQP2016/src/main/scala/Test.scala
+++ ionicMQP2016/src/main/scala/Test.scala
@@ -52,22 +52,24 @@ object TestExample extends App {
       val semanticType:Type = 'color :&: colorVar =>: colorVar :&: b
     }
 
-    class Bind(thing:Symbol, fileName: String) {
-      def apply(expr:String) : Tuple = { return new Tuple(expr, fileName); }
-      val semanticType:Type = thing =>: 'BoundFile :&: thing
+    // Binds a symbol to a file, so it can be written out by Java
+    class Bind(sym:Symbol, fileName:String) {
+      def apply(expr:String): Tuple = {
+        return new Tuple(expr, fileName);
+      }
+      val semanticType:Type = sym =>: 'BoundFile :&: sym
     }
 
     @combinator object Comb1 extends Dyer('color, 'shirt)
     @combinator object Comb2 extends Painter('color, 'car)
 
-    @combinator object Bind1 extends Bind('shirt, "TestDirectory/www/js/Shirt.txt")
-    @combinator object Bind2 extends Bind('car, "TestDirectory/www/js/Car.txt")
+    @combinator object Bind1 extends Bind('shirt, "www/js/Shirt.txt")
+    @combinator object Bind2 extends Bind('car, "www/js/Car.txt")
 
   }
 
   // Initializes the CLS system
-  val repository = new TestTrait {}
-  val reflectedRepository = ReflectedRepository (repository, kinding=repository.kinding)
+  val reflectedRepository = ReflectedRepository (new TestTrait {}, kinding=repository.kinding)
 
   // Get the interpreted response from CLS
   val reply = reflectedRepository.inhabit[Tuple] ('BoundFile)
