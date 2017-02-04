bff9be735f23ff610ed56680af26c7f86f631a3b
Bailey <bsheridan@wpi.edu>
2017-02-04 16:20:16 -0500

    Converts HTML chunks to twirl templates

--- cls_sbt/src/main/scala/AppCreator.scala
+++ cls_sbt/src/main/scala/AppCreator.scala
@@ -4,6 +4,7 @@ import de.tu_dortmund.cs.ls14.cls.types._
 import de.tu_dortmund.cs.ls14.cls.interpreter.combinator
 
 import play.twirl.api.JavaScript
+import play.twirl.api.Html
 import scala.collection.JavaConverters._
 import ionicmqp2016._;
 import _root_.java.nio.file._
@@ -21,56 +22,42 @@ object AppCreator extends App {
       .addOption('dummy)
       .addOption('monster)
 
-    @combinator object MastermindHTML {
-      def apply(button:buttonType): String = {
-        return """
-      <div class="row">
-        <input class="item item-input" maxLength="{{word.length}}" id="guess_{{levelNum}}" placeholder="Guess a {{word.length}} letter word">
-        &nbsp; """ + button("makeGuess", "Guess", "positive") + """
-      </div>
-      <div class="row">
-        <h4>{{result}}</h4>
-      </div>"""
-      }
-      val semanticType:Type = 'button =>: 'mastermind :&: 'html
-    }
-
-    type buttonType = (String, String, String) => String
+    type buttonType = (String, String, String) => Html
     @combinator object Button {
       def apply(): buttonType = {
         return (callback:String, text:String, color:String) =>
-          s"""<button class="button button-$color levelBtn" ng-click="$callback()">$text</button>"""
+          new Html(s"""<button class="button button-$color levelBtn" ng-click="$callback()">$text</button>""")
       }
       val semanticType:Type = 'button
     }
 
-    type toggleType = (String, String) => String
+    type toggleType = (String, String) => Html
     @combinator object Toggle {
       def apply(): toggleType = {
         return (model:String, name:String) =>
-          s"""$name<label class="toggle">
+          new Html(s"""$name<label class="toggle">
            <input type="checkbox" ng-model="$model">
            <div class="track">
              <div class="handle"></div>
            </div>
-        </label>"""
+        </label>""")
       }
       val semanticType:Type = 'toggle
     }
 
-    type rangeType = (Int, Int, String, String, String, String, String) => String
+    type rangeType = (Int, Int, String, String, String, String, String) => Html
     @combinator object Range {
       def apply(): rangeType = {
         return (min:Int, max:Int, iconLeft:String, iconRight:String, name:String, model:String, callback:String) =>
-          s"""<div class="item range">
+          new Html(s"""<div class="item range">
           <i class="icon $iconLeft"></i>
           <input type="range" name="$name" min="$min" max="$max" ng-model="$model" ng-change="$callback($model)">
-          <i class="icon $iconRight"></i></div>"""
+          <i class="icon $iconRight"></i></div>""")
       }
       val semanticType:Type = 'range
     }
 
-    type checkboxType = (Array[String], Array[String]) => String
+    type checkboxType = (Array[String], Array[String]) => Html
     @combinator object Checkboxes {
       def apply(): checkboxType = {
         def buildChoices(models:Array[String], choices:Array[String]): String = {
@@ -81,12 +68,12 @@ object AppCreator extends App {
           }
           return a;
         }
-        return (models:Array[String], choices:Array[String]) => "<ion-list>" + buildChoices(models, choices) + "</ion-list>"
+        return (models:Array[String], choices:Array[String]) => new Html("<ion-list>" + buildChoices(models, choices) + "</ion-list>")
       }
       val semanticType:Type = 'checkboxes
     }
 
-    type radioType = (String, Array[String], Array[String]) => String
+    type radioType = (String, Array[String], Array[String]) => Html
     @combinator object RadioButtons {
       def apply(): radioType = {
         def buildChoices(model:String, values:Array[String], choices:Array[String]): String = {
@@ -97,107 +84,63 @@ object AppCreator extends App {
           }
           return a;
         }
-        return (model:String, values:Array[String], choices:Array[String]) => "<ion-list>" + buildChoices(model, values, choices) + "</ion-list>"
+        return (model:String, values:Array[String], choices:Array[String]) => new Html("<ion-list>" + buildChoices(model, values, choices) + "</ion-list>")
       }
       val semanticType:Type = 'radiobuttons
     }
 
+    @combinator object MastermindHTML {
+      def apply(button:buttonType): String = {
+        return html.html.mastermind.render(button).toString()
+      }
+      val semanticType:Type = 'button =>: 'mastermind :&: 'html
+    }
+
     @combinator object HangmanHTML {
       def apply(button:buttonType): String = {
-        return """
-      <div class= "item row">
-    		<div ng-repeat="letter in myLetters track by $index">
-    			<div class= "guessable {{levelNum}} {{letter}}">{{letter}}</div>
-    		</div>
-    	</div>
-    	<br>
-    	<div class="row">
-    		<ion-input class="item item-input item-stacked-label">
-    			<input type="text" maxLength="1" class= "letterguess" id="letterguess_{{levelNum}}" placeholder="Guess a letter here">
-    		</ion-input>
-    		&nbsp; """ + button("makeGuess", "Guess", "positive") + """
-    	</div>
-
-    	<h4>Guessed Letters:</h4>
-        <div class = "row" id="guessed_{{levelNum}}"></div>
-      <h4>Tries Left: {{guessesLeft}}</h4>"""
+        return html.html.hangman.render(button).toString()
       }
       val semanticType:Type = 'button =>: 'hangman :&: 'html
     }
 
     @combinator object LightsOutHTML {
       def apply(): String = {
-        return """
-      <div class="row" ng-repeat="row in buttons">
-        <div class="col col-25"></div>
-        <div class="col" ng-repeat="button in row">
-          <a class="button button-dark" ng-click="toggle('{{button}}')" id="{{levelNum}}_{{button}}">&nbsp</a>
-        </div>
-        <div class="col col-25"></div>
-      </div>"""
+        return html.html.lightsout.render().toString()
       }
       val semanticType:Type = 'lightsout :&: 'html
     }
 
     @combinator object DummyHTML {
       def apply(): String = {
-        return """
-      <div class="col" style="text-align:center">
-        <button class="button button-assertive levelBtn" ng-click="completeLevel()">Click Me!</button>
-      </div>"""
+        return html.html.dummy.render().toString()
       }
       val semanticType:Type = 'dummy :&: 'html
     }
 
-    @combinator object FrankensteinGame {
-      def apply(radiobuttons:radioType, checkboxes:checkboxType, button:buttonType, toggle:toggleType, range:rangeType): String = {
-        return """
-      <div class="row">
-        <div class="col" style="text-align:center">
-        """+toggle("frankenVars.toggle", "Alive?")+"""
-        </div>
-      </div>
-      <div class="row">
-        <div class="col" style="text-align:center">
-        """+radiobuttons("frankenVars.rBox", Array("'Check Me'", "'Do not'", "'Do not'"), Array("Check Me", "Do not", "Do not"))+"""
-        </div>
-      </div>
-      <div class="row">
-        <div class="col" style="text-align:center">
-        """+checkboxes(Array("frankenVars.cBox1", "frankenVars.cBox2", "frankenVars.cBox3"), Array("REEEED", "GREEEEEN", "Blue"))+"""
-        </div>
-      </div>
-      <div class="row">
-        <div class="col" style="text-align:center">
-        """+range(0, 75, "ion-flash-off", "ion-flash", "power", "frankenVars.range1", "rangeCallbackFcn")+"""
-        </div>
-      </div>
-      <div class="row">
-        <div class="col" style="text-align:center">
-        """+button("checkComplete", "Click Me!", "assertive")+"""
-        </div>
-      </div>"""
+    @combinator object FrankensteinHTML {
+      def apply(radioButtons:radioType, checkBoxes:checkboxType, endButton:buttonType, toggler:toggleType, rangey:rangeType): String = {
+        return html.html.frankenstein.render(radioButtons, checkBoxes, endButton, toggler, rangey).toString()
       }
       val semanticType:Type = 'radiobuttons =>: 'checkboxes =>: 'button =>: 'toggle =>: 'range =>: 'monster :&: 'html
     }
 
     @combinator object GameHTML {
       def apply(contents:String): String = {
-        return html.game.render(contents).toString()
+        return html.html.game.render(contents).toString()
       }
       val semanticType:Type = gameVar :&: 'html =>: gameVar :&: 'gameHtml
     }
 
     @combinator object GameJs {
       def apply(contents:JavaScript): String = {
-        return js.game.render(contents).toString()
+        return js.js.game.render(contents).toString()
       }
       val semanticType:Type = gameVar :&: 'js =>: gameVar :&: 'gameJs
     }
 
     @combinator object MainPage {
       def apply(gameTitle:String): String = {
-        return html.mainpage.render(gameTitle).toString()
+        return html.html.mainpage.render(gameTitle).toString()
       }
       val semanticType:Type = gameVar :&: 'gameTitle =>: 'mainPage :&: gameVar
     }
@@ -248,14 +191,14 @@ object AppCreator extends App {
 
     @combinator object IndexHTML {
       def apply(scriptList:Array[String]): String = {
-        return html.index.render(scriptList).toString()
+        return html.html.index.render(scriptList).toString()
       }
       val semanticType:Type = 'scriptList =>: 'indexHtml
     }
 
     @combinator object AppJs {
       def apply(scriptList:Array[String]): String = {
-        return js.app.render(scriptList).toString()
+        return js.js.app.render(scriptList).toString()
       }
       val semanticType:Type = 'scriptList =>: 'appJs
     }
@@ -269,7 +212,7 @@ object AppCreator extends App {
 
     @combinator object States {
       def apply(stateList:Array[String]): String = {
-        return js.states.render(stateList).toString()
+        return js.js.states.render(stateList).toString()
       }
       // List of states is also list of scripts
       val semanticType:Type = 'stateList =>: 'states
@@ -277,14 +220,14 @@ object AppCreator extends App {
 
     @combinator object Controllers {
       def apply(levelList:Array[Int]): String = {
-        return js.controllers.render(levelList).toString()
+        return js.js.controllers.render(levelList).toString()
       }
       val semanticType:Type = 'levelList =>: 'controllers
     }
 
     @combinator object SQL {
       def apply(sqlcolumns:Map[String, String]): String = {
-        return js.sql.render(sqlcolumns).toString()
+        return js.js.sql.render(sqlcolumns).toString()
       }
       val semanticType:Type = 'sqlcolumns =>: 'sql
     }
@@ -303,14 +246,14 @@ object AppCreator extends App {
       val semanticType:Type = inputType
     }
 
-    @combinator object LevelSelect extends Render('levelSelect, html.levelselect.render().toString())
-    @combinator object Settings extends Render('settings, html.settings.render().toString())
+    @combinator object LevelSelect extends Render('levelSelect, html.html.levelselect.render().toString())
+    @combinator object Settings extends Render('settings, html.html.settings.render().toString())
 
-    @combinator object HangmanJS extends RenderJS('hangman :&: 'js, js.hangman.render())
-    @combinator object MastermindJS extends RenderJS('mastermind :&: 'js, js.mastermind.render())
-    @combinator object LightsOutJS extends RenderJS('lightsout :&: 'js, js.lightsout.render())
-    @combinator object DummyJS extends Render('dummy :&: 'js, "")
-    @combinator object FrankensteinJS extends RenderJS('monster :&: 'js, js.frankenstein.render())
+    @combinator object HangmanJS extends RenderJS('hangman :&: 'js, js.js.hangman.render())
+    @combinator object MastermindJS extends RenderJS('mastermind :&: 'js, js.js.mastermind.render())
+    @combinator object LightsOutJS extends RenderJS('lightsout :&: 'js, js.js.lightsout.render())
+    @combinator object DummyJS extends RenderJS('dummy :&: 'js, js.js.dummy.render())
+    @combinator object FrankensteinJS extends RenderJS('monster :&: 'js, js.js.frankenstein.render())
 
     class Bind(inputType:Type, filePath:String){
       def apply(expr:String) : Tuple = {
@@ -341,7 +284,7 @@ object AppCreator extends App {
   val reflectedRepository = ReflectedRepository (repository, kinding=repository.kinding)
 
   // Get the interpreted response from CLS
-  val reply = reflectedRepository.inhabit[Tuple] ('BoundFile :&: 'lightsout)
+  val reply = reflectedRepository.inhabit[Tuple] ('BoundFile :&: 'monster)
 
   // Pass the response into our defined output, currently just a printer
   val iter = reply.interpretedTerms.values.flatMap(_._2).iterator.asJava
