f24bbfeb302fb909bc03cb6ef19ed24017448be8
jbzdarkid <jbzdarkid@gmail.com>
2017-02-18 16:00:58 -0500

    Step 3b: Add win condition

--- cls_sbt/src/main/templates/js/nim.scala.js
+++ cls_sbt/src/main/templates/js/nim.scala.js
@@ -12,3 +12,28 @@ $scope.initializeLevel = function() {
     setSlider(i)
   }
 }
+
+$scope.callback = function(slider) {
+  var i = parseInt(slider.split('_')[2])
+  var slider = document.getElementsByName('slider_'+$scope.levelNum+'_'+i)[0]
+  var value = parseInt(slider.value)
+  if ($scope.sliders[i] == value) {
+    return // User didn't change the slider
+  } else {
+    $scope.sliders[i] = value
+  }
+  setSlider(i)
+
+  if ($scope.checkComplete()) {
+    $scope.completeLevel(true)
+  }
+}
+
+$scope.checkComplete = function() {
+  for (var i=0; i<$scope.sliders.length; i++) {
+    if ($scope.sliders[i] != 0) {
+      return false
+    }
+  }
+  return true
+}
