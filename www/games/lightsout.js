73fb034412d7adee14c89403a5d1a30e45c4d183
djkiourtsis <djkiourtsis@wpi.edu>
2017-02-25 17:28:05 -0500

    Adds completed lightsout game

--- www/games/lightsout.js
+++ www/games/lightsout.js
@@ -1,133 +1,222 @@
-angular.module("lightsout", ["ionic", "sql"])
 
-.controller("LevelCtrl", function ($scope, $rootScope, $state, $stateParams, $ionicPopup, sqlfactory) {
-  // Check for invalid level number
-  if ($rootScope.levels === undefined) {
-    console.log("Level loaded but level list undefined, going to main");
-    $state.go("main");
-  } else if (!$rootScope.levels.includes($stateParams.levelNum)) {
-    console.log("Went to level " + $stateParams.levelNum + " redirecting to level select.");
-    $state.go("level_select");
+angular.module("game", ["ionic", "sql", "dictionary"])
+
+.controller("LevelCtrl", function ($scope, $rootScope, $ionicPopup, $state, $stateParams, sqlfactory, dictionary) {
+  // Check for invalid state
+  if ($rootScope.levelData === undefined) {
+    console.log("Level loaded but level list undefined, going to main")
+    $state.go("main")
   } else {
-    console.log("Now in level: " + $stateParams.levelNum);
+    try {
+      // Redefined so that they can be used in the HTML
+      $scope.levelNum = $stateParams.levelNum
+      $scope.levelName = $rootScope.levelData[$scope.levelNum]["name"]
+      console.log("Now in " + $scope.levelName)
+    } catch (err) {
+      console.log("Tried to go to " + $scope.levelNum + ", redirecting to level select.")
+      $state.go("level_select")
+    }
   }
-
-  // Redefined so that it can be used in the HTML.
-  $scope.levelNum = $stateParams.levelNum;
 
   // Used by the HTML to name the buttons
-  $scope.buttons = [
-    ["0_0", "0_1", "0_2", "0_3", "0_4", "0_5"],
-    ["1_0", "1_1", "1_2", "1_3", "1_4", "1_5"],
-    ["2_0", "2_1", "2_2", "2_3", "2_4", "2_5"],
-    ["3_0", "3_1", "3_2", "3_3", "3_4", "3_5"],
-    ["4_0", "4_1", "4_2", "4_3", "4_4", "4_5"],
-    ["5_0", "5_1", "5_2", "5_3", "5_4", "5_5"]
-  ];
-
-  function convertTo(longState) {
-    arr = [];
-    for (var i = 0; i < 6; i++) {
-      row = [];
-      for (var j = 0; j < 6; j++) {
-        row.unshift(longState%2);
-        longState = Math.floor(longState/2);
-      }
-      arr.unshift(row);
-    }
-    return arr;
+$scope.buttons = []
+for (var i=0; i<6; i++) {
+  var row = []
+  for (var j=0; j<6; j++) {
+    row[j] = i+"_"+j
   }
+  $scope.buttons[i] = row
+}
+// $scope.buttons = [
+//   ["0_0", "0_1", "0_2", "0_3", "0_4", "0_5"],
+//   ["1_0", "1_1", "1_2", "1_3", "1_4", "1_5"],
+//   ["2_0", "2_1", "2_2", "2_3", "2_4", "2_5"],
+//   ["3_0", "3_1", "3_2", "3_3", "3_4", "3_5"],
+//   ["4_0", "4_1", "4_2", "4_3", "4_4", "4_5"],
+//   ["5_0", "5_1", "5_2", "5_3", "5_4", "5_5"]
+// ]
 
-  function convertFrom(arr) {
-    longState = 0;
-    for (var i = 0; i < 6; i++) {
-      for (var j = 0; j < 6; j++) {
-        longState *= 2;
-        longState += arr[i][j];
+function convertTo(longState) {
+  arr = []
+  for (var i = 0; i < 6; i++) {
+    row = []
+    for (var j = 0; j < 6; j++) {
+      row.unshift(longState%2)
+      longState = Math.floor(longState/2)
+    }
+    arr.unshift(row)
+  }
+  return arr
+}
+
+function convertFrom(arr) {
+  longState = 0
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 6; j++) {
+      longState *= 2
+      longState += arr[i][j]
+    }
+  }
+  return longState
+}
+
+$scope.beforeLeave = function () {
+  var state = convertFrom($scope.buttonsList)
+  //console.log("State before leave: " + $scope.buttonsList)
+  sqlfactory.setLevelState($scope.levelNum, state)
+}
+
+$scope.initializeLevel = function () {
+  if ($rootScope.levelData[$scope.levelNum]["state"] == "Unsolved" ||
+      $rootScope.levelData[$scope.levelNum]["state"] == 0) {
+    var defaultState = [
+      18123587584, 18119623680, 480887296, 8738341376, 315360000,
+       2684356643,      524288,  34873344,  807600128, 137561088,
+                0,           0,         0,          0,         0,
+                0,           0,         0,          0,         0
+    ][$scope.levelNum]
+    sqlfactory.setLevelState($scope.levelNum, defaultState)
+  }
+  $scope.buttonsList = convertTo($rootScope.levelData[$scope.levelNum]["state"])
+  console.log("State for "+$scope.levelName+": "+$scope.buttonsList)
+  for (var row=0; row<6; row++) {
+    for (var col=0; col<6; col++) {
+      var name = $scope.levelNum + "_" + row + "_" + col
+      var button = document.getElementById(name)
+      if (button === null) {
+        console.log("Couldn't find button "+name)
+      } else if ($scope.buttonsList[row][col] == 1) {
+        button.className = "button button-energized"
+      } else {
+        button.className = "button button-dark"
       }
     }
-    return longState;
   }
+}
 
-  // test = 23587584;
-  // console.log(test);
-  // test = convertTo(test);
-  // console.log(""+test);
-  // test = convertFrom(test);
-  // console.log(test);
+$scope.toggleButton = function (row, col) {
+  var name = $scope.levelNum + "_" + row + "_" + col
+  var button = document.getElementById(name)
+  if (button === null) {
+    return
+  }
+  //console.log(button.className)
+  if ($scope.buttonsList[row][col] == 1) {
+    button.className = "button button-dark"
+    $scope.buttonsList[row][col] = 0
+  } else if ($scope.buttonsList[row][col] == 0) {
+    button.className = "button button-energized"
+      $scope.buttonsList[row][col] = 1
+  }
+}
 
-  // This runs whenever a level is entered
+// Logic for toggling lights.
+$scope.toggle = function (button_name) {
+  // All of our buttons are named "#_#", so if the table was 10 rows or 10
+  // columns, this logic would need to change.
+  var row = parseInt(button_name.charAt(0))
+  var col = parseInt(button_name.charAt(2))
+
+  $scope.toggleButton(row, col)
+  $scope.toggleButton(row, col+1)
+  $scope.toggleButton(row, col-1)
+  $scope.toggleButton(row+1, col)
+  $scope.toggleButton(row-1, col)
+
+  // Check for game completion
+  for (var row=0; row < $scope.buttonsList.length; row++) {
+    for (var col=0; col < $scope.buttonsList[0].length; col++) {
+      var name = $scope.levelNum + "_" + row + "_" + col
+      var button = document.getElementById(name)
+      if (button.className.includes("button-energized")) {
+        // Found a button which wasn't off, level not complete
+        return
+      }
+    }
+  }
+  console.log("All lights out, completing level.")
+  $scope.completeLevel(true)
+}
+
+
+  // This runs when the level is entered
   $scope.$on("$ionicView.afterEnter", function(scopes, states){
-    console.log("Entered "+states.stateName+" "+$stateParams.levelNum);
-    $scope.buttonsList = convertTo($rootScope.states[$stateParams.levelNum-1]);
-    console.log("State for level "+$stateParams.levelNum + ":"+$scope.buttonsList);
-    for (var row=0; row<6; row++) {
-      for (var col=0; col<6; col++) {
-        var name = $stateParams.levelNum + "_" + row + "_" + col;
-        var button = document.getElementById(name);
-        if (button === null) {
-          console.log("Couldn't find button "+name);
-        } else if ($scope.buttonsList[row][col] == 1) {
-          button.className = "button button-energized";
-        } else {
-          button.className = "button button-dark";
+    console.log("Entered "+$scope.levelName+":", $rootScope.levelData[$scope.levelNum])
+    
+      $rootScope.levelData[$scope.levelNum]["time"] -= Date.now()
+    
+    if (typeof $scope.initializeLevel === "function") {
+      $scope.initializeLevel()
+    }
+  })
+
+  // This runs when the level is exited
+  $scope.$on("$ionicView.beforeLeave", function(scopes, states){
+    console.log("Exited "+states.stateName+" "+$scope.levelNum+":", $rootScope.levelData[$scope.levelNum])
+    $rootScope.levelData[$scope.levelNum]["time"] += Date.now()
+    if (typeof $scope.beforeLeave === "function") {
+      $scope.beforeLeave()
+    }
+  })
+
+  $scope.completeLevel = function(won) {
+    
+      var time = $rootScope.levelData[$scope.levelNum]["time"] + Date.now()
+    
+    if (won) {
+      sqlfactory.setLevelState($scope.levelNum, "Solved", 0)
+      button = document.getElementById("level_"+$scope.levelNum)
+      button.setAttribute("class", "button button-dark ng-binding")
+      
+        var title = $scope.levelName + " Complete! Total time: " + time / 1000 + " seconds"
+      
+      if(typeof $scope.onWin === "function") {
+ 			  $scope.onWin()
+ 			}
+      var levelOption = {
+        text: "Next",
+        type: "button-positive",
+        onTap: function () {
+          console.log("On to the next level.")
+          $state.go("level", {"levelNum": $scope.levelNum+1})
         }
       }
-    }
-  });
-
-  $scope.$on("$ionicView.beforeLeave", function(scope, state){
-    console.log("Exiting "+state.stateName+" "+$stateParams.levelNum);
-    state = convertFrom($scope.buttonsList);
-    $rootScope.states[$stateParams.levelNum-1] = state;
-    sqlfactory.setLevelState($stateParams.levelNum, state);
-  });
-
-  $scope.toggleButton = function (row, col) {
-    var name = $stateParams.levelNum + "_" + row + "_" + col;
-    var button = document.getElementById(name);
-    if (button === null) {
-      return;
-    }
-    //console.log(button.className);
-    if ($scope.buttonsList[row][col] == 1) {
-      button.className = "button button-dark";
-      $scope.buttonsList[row][col] = 0;
     } else {
-      $scope.buttonsList[row][col] = 1;
-      button.className = "button button-energized";
-    }
-  }
-
-  // Logic for toggling lights. This can be simplified if toggling uses the number, not the array: completion is just compare to 0.
-  $scope.toggle = function (button_name) {
-    // All of our buttons are named "#_#", so if the table was 10 rows or 10
-    // columns, this logic would need to change.
-    var row = parseInt(button_name.charAt(0));
-    var col = parseInt(button_name.charAt(2));
-
-    $scope.toggleButton(row, col);
-    $scope.toggleButton(row, col+1);
-    $scope.toggleButton(row, col-1);
-    $scope.toggleButton(row+1, col);
-    $scope.toggleButton(row-1, col);
-
-    // Check for game completion
-    for (var row=0; row < $scope.buttonsList.length; row++) {
-      for (var col=0; col < $scope.buttonsList[0].length; col++) {
-        if ($scope.buttonsList[row][col] != 0) {
-          // Found a button which wasn't off, level not complete
-          return;
+      var title = "You lose!"
+      if(typeof $scope.onLose === "function") {
+        $scope.onLose()
+      }
+      var levelOption = {
+        text: "Retry",
+        type: "button-positive",
+        onTap: function() {
+          $scope.restart()
         }
       }
     }
-    console.log("All lights out, completing level.")
-    $rootScope.completeLevel($state, $stateParams.levelNum);
-  }
+
+     $ionicPopup.show({
+       title: title,
+       buttons: [
+       {
+         text: "Level Select",
+         onTap: function () {
+           console.log("Back to level select.")
+           $state.go("level_select")
+         }
+       }, levelOption]
+     })
+   }
 
   $scope.restart = function () {
-    console.log("Restarting level...");
-    $rootScope.states[$stateParams.levelNum-1] = $rootScope.defaultStates[$stateParams.levelNum-1];
-    $state.reload();
+    console.log("Restarting level...")
+    sqlfactory.setLevelState($scope.levelNum, "")
+    
+      $rootScope.levelData[$scope.levelNum]["time"] += Date.now()
+    
+    if(typeof $scope.restartLevel === "function") {
+      $scope.restartLevel()
+    }
+    $state.reload()
   }
-});
+})
