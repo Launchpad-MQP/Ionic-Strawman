70bd88e84574781fe9ac3448ce7eebead7cdd3d6
Bailey <bsheridan@wpi.edu>
2017-02-25 14:56:18 -0500

    Fixes level solve bugs in hangman

--- cls_sbt/src/main/templates/js/hangman.scala.js
+++ cls_sbt/src/main/templates/js/hangman.scala.js
@@ -1,80 +1,93 @@
-$scope.myLetters = [
+  $scope.myLetters = [
   'hello', 'wolf', 'rat', 'xylophone', 'attention',
   'school', 'ruling', 'poison', 'tree', 'prison',
   'abacus', 'toothache', 'short', 'bacon', 'crossroads',
   'darkness', 'candle', 'quadruple', 'extraordinary', 'declaration'
-][$scope.levelNum].split("")
-$scope.guessesLeft = 7
-$scope.miss = true
+  ][$scope.levelNum].split("")
+  $scope.guessesLeft = 7
+  $scope.miss = true
+  $scope.finished = false
 
-$scope.makeGuess = function () {
- var guessBox = document.getElementById("letterguess_" + $scope.levelNum)
- var guess = guessBox.value
- var card = document.getElementById("guessed_" + $scope.levelNum)
+  $scope.makeGuess = function () {
+  var guessBox = document.getElementById("letterguess_" + $scope.levelNum)
+  var guess = guessBox.value
+  var card = document.getElementById("guessed_" + $scope.levelNum)
 
- console.log("Guess", guess)
- // Fields are the elements which match the letter guessed.
- var fields = document.getElementsByClassName(guess)
- console.log("Fields", fields)
- for(var i = 0; i<fields.length; i++) {
+  console.log("Guess", guess)
+  // Fields are the elements which match the letter guessed.
+  var fields = document.getElementsByClassName(guess)
+  console.log("Fields", fields)
+  for(var i = 0; i<fields.length; i++) {
      if(!fields[i].className.includes("discovered")) {
        fields[i].className = "discovered " + $scope.levelNum + " " + guess
      }
      $scope.miss = false
- }
- if($scope.miss) {
+  }
+  if($scope.miss) {
    if(!card.innerHTML.includes(guess)) {
      $scope.guessesLeft--
      card.append(guess + " ")
    }
    if($scope.guessesLeft==0)
      $scope.completeLevel(false)
- } else {
+  } else {
    if(!card.innerHTML.includes(guess))
      card.append(guess + " ")
    $scope.checkComplete()
- }
- guessBox.value = ""
- $scope.miss = true
-}
+  }
+  guessBox.value = ""
+  $scope.miss = true
+  }
 
-$scope.checkComplete = function () {
- var correct = document.getElementsByClassName("discovered " + $scope.levelNum)
- console.log(correct.length)
- if(correct.length === $scope.myLetters.length)
+  $scope.checkComplete = function () {
+  var correct = document.getElementsByClassName("discovered " + $scope.levelNum)
+  console.log(correct.length)
+  if(correct.length === $scope.myLetters.length)
    $scope.completeLevel(true)
-}
+  }
 
-$scope.beforeLeave = function() {
-	var card = document.getElementById("guessed_" + $scope.levelNum)
-	var guessed = card.innerHTML.split(", ")
-  sqlfactory.setLevelState($stateParams.levelNum, guessed)
-}
+  $scope.beforeLeave = function() {
+  var card = document.getElementById("guessed_" + $scope.levelNum)
+  var guessed = card.innerHTML.split(", ")
+  console.log("before leaving")
+  console.log(guessed)
+  if(!$scope.finished)
+    sqlfactory.setLevelState($stateParams.levelNum, guessed[0])
+  }
 
-$scope.onLose = function() {
- console.log("losing")
- document.getElementById("guessed_" + $scope.levelNum).innerHTML = "";
-}
+  $scope.onWin = function() {
+  console.log("On Win")
+  $scope.finished = true
+  sqlfactory.setLevelState($stateParams.levelNum, "Solved", 0)
+  }
 
-$scope.initializeLevel = function() {
- $scope.guessesLeft = 7
- $scope.miss = true
- var card = document.getElementById("guessed_" + $scope.levelNum)
- var guessBox = document.getElementById("letterguess_" + $scope.levelNum)
- card.innerHTML = ""
- guessBox.value = ""
- var fields = document.getElementsByClassName($scope.levelNum)
- for(var i = 0; i<fields.length; i++) {
+  $scope.onLose = function() {
+  console.log("losing")
+  sqlfactory.setLevelState($stateParams.levelNum, "Lost")
+  document.getElementById("guessed_" + $scope.levelNum).innerHTML = "";
+  }
+
+  $scope.initializeLevel = function() {
+  $scope.guessesLeft = 7
+  $scope.miss = true
+  var card = document.getElementById("guessed_" + $scope.levelNum)
+  var guessBox = document.getElementById("letterguess_" + $scope.levelNum)
+  card.innerHTML = ""
+  guessBox.value = ""
+  var fields = document.getElementsByClassName($scope.levelNum)
+  for(var i = 0; i<fields.length; i++) {
    var letter = fields[i].className.slice(-1)
    fields[i].className = "guessable " + $scope.levelNum + " " + letter
- }
+  }
 
-  if ($rootScope.levelData[$stateParams.levelNum]["state"] != "Unsolved") {
-	  var letters = $rootScope.levelData[$stateParams.levelNum]["state"].split("")
+  if ($rootScope.levelData[$stateParams.levelNum]["state"] != "Unsolved" &&
+        $rootScope.levelData[$stateParams.levelNum]["state"] != "Solved") {
+    console.log($rootScope.levelData[$stateParams.levelNum]["state"])
+    var letters = $rootScope.levelData[$stateParams.levelNum]["state"].split("")
 
-	  for (var j = 0; j < letters.length; j++) {
-	    guessBox.value = letters[j]
-	    $scope.makeGuess()
-	  }
-	}
+    for (var j = 0; j < letters.length; j++) {
+      guessBox.value = letters[j]
+      $scope.makeGuess()
+    }
+  }
 }
